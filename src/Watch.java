efc9312515c87679c3b4defbb58932bf4b3c954a
gerings17 <skyswims@yahoo.com>
2017-02-09 16:59:08 -0800

    FEB 9 2017

--- src/Watch.java
+++ src/Watch.java
@@ -1,13 +1,13 @@
 
 public class Watch {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Movie spilgrim = new Movie("Scott Pilgrim vs The World", 5);
 		Movie avengers = new Movie("The Avengers", 4);
-		Movie sw = new Movie("Star Wars: A New Hope", 5);
+		Movie sw = new Movie("Star Wars: A New Hope", 4);
 		Movie airbender = new Movie("Avatar: The Last Airbender", 2);
 		Movie wchicks = new Movie("White Chicks", 1);
-		System.out.println(sw.getTicketPrice());
+		System.out.println(wchicks.getTicketPrice());
 		NetflixQueue queue = new NetflixQueue();
 		queue.addMovie(sw);
 		queue.addMovie(wchicks);
@@ -15,8 +15,8 @@ public class Watch {
 		queue.addMovie(avengers);
 		queue.addMovie(spilgrim);
 		queue.printMovies();
-		System.out.println("the best movie is "+queue.getBestMovie());
-		System.out.println("the second best movie is "+queue.movies.get(1));
+		System.out.println("the best movie is " + queue.getBestMovie());
+		System.out.println("the second best movie is " + queue.movies.get(1));
 	}
-	
+
 }
